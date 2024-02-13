3bae38bf0827a4454d1460fb488857b198ddde71
David Cardona <david.cardonanieves@mercadolibre.com.com>
2024-02-13 14:15:19 -0500

    Solucion ejercicio

---
+++ src/main/java/me/davidlake/ejerciciostarwars/service/CharacterService.java
@@ -0,0 +1,32 @@
+package me.davidlake.ejerciciostarwars.service;
+
+import me.davidlake.ejerciciostarwars.dto.CharacterDTO;
+import me.davidlake.ejerciciostarwars.entity.Character;
+import me.davidlake.ejerciciostarwars.repository.CharacterRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CharacterService {
+    private final CharacterRepository characterRepository;
+    private final ModelMapper modelMapper;
+
+    public CharacterService(
+            CharacterRepository characterRepository,
+            ModelMapper modelMapper
+    ) {
+        this.characterRepository = characterRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<CharacterDTO> searchByName(String name) {
+        List<Character> matched = characterRepository.searchByName(name);
+
+        return matched
+                .stream()
+                .map(character -> modelMapper.map(character, CharacterDTO.class))
+                .toList();
+    }
+}
